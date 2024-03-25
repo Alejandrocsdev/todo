229dac1780387a2116eb4578d575a0f5cc85d882
Alejandrocsdev <alejandrocsdev@gmail.com>
2024-03-25 20:29:40 +0800

    router init

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/todos', (req, res) => {
+	res.send('get all todos')
+})
+
+app.get('/todos/new', (req, res) => {
+	res.send('create todo')
+})
+
+app.post('/todos', (req, res) => {
+	res.send('add todo')
+})
+
+app.get('/todos/:id', (req, res) => {
+	res.send(`get todo: ${req.params.id}`)
+})
+
+app.get('/todos/:id/edit', (req, res) => {
+	res.send(`get todo edit: ${req.params.id}`)
+})
+
+app.put('/todos/:id', (req, res) => {
+	res.send('modify todo')
+})
+
+app.delete('/todos/:id', (req, res) => {
+	res.send('delete todo')
+})
