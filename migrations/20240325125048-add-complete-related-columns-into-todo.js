6b7173787d4a445c2c73781e7994e9c601457403
Alejandrocsdev <alejandrocsdev@gmail.com>
2024-03-25 20:57:05 +0800

    C4M2: create model migration

---
+++ migrations/20240325125048-add-complete-related-columns-into-todo.js
@@ -0,0 +1,20 @@
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.addColumn(
+      'Todos',
+      'isComplete',
+      {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+        allowNull: false
+      }
+    )
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.removeColumn(
+      'Todos',
+      'isComplete'
+    )
+  }
+};
--- models/todo.js
+++ models/todo.js
@@ -1,23 +1,22 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+'use strict'
+const { Model } = require('sequelize')
 module.exports = (sequelize, DataTypes) => {
   class Todo extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
+    static associate(models) {}
   }
-  Todo.init({
-    name: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Todo',
-  });
-  return Todo;
-};
+  Todo.init(
+    {
+      name: DataTypes.STRING,
+      isComplete: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+        allowNull: false
+      }
+    },
+    {
+      sequelize,
+      modelName: 'Todo'
+    }
+  )
+  return Todo
+}
