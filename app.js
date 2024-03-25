54058c63c7228837da3ad40307669a0cdda6e895
Alejandrocsdev <alejandrocsdev@gmail.com>
2024-03-25 21:45:16 +0800

    use express router

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const { engine } = require('express-handlebars')
 const methodOverride = require('method-override')
 const flash = require('connect-flash')
 const session = require('express-session')
+// ROUTES
+const router = require('./routes') // 引用路由器
 // EXPRESS
 const app = express()
 // SERVER
@@ -24,67 +26,7 @@ app.use(session({
 	saveUninitialized: false
 }))
 app.use(flash())
-
-app.get('/', (req, res) => {
-  res.render('index')
-})
-
-app.get('/todos', (req, res) => {
-	return Todo.findAll({
-		attributes: ['id', 'name', 'isComplete'],
-		raw: true
-	})
-		.then((todos) => res.render('todos', { todos, message: req.flash('success') }))
-})
-
-app.get('/todos/new', (req, res) => {
-  return res.render('new')
-})
-
-app.post('/todos', (req, res) => {
-	const name = req.body.name
-
-	return Todo.create({ name })
-		.then(() => {
-			req.flash('success', '新增成功!')
-			return res.redirect('/todos')
-		})
-})
-
-app.get('/todos/:id', (req, res) => {
-  const id = req.params.id
-
-  return Todo.findByPk(id, {
-    attributes: ['id', 'name', 'isComplete'],
-    raw: true
-  })
-    .then((todo) => res.render('todo', { todo }))
-    .catch((err) => console.log(err))
-})
-
-app.get('/todos/:id/edit', (req, res) => {
-  const id = req.params.id
-
-  return Todo.findByPk(id, {
-    attributes: ['id', 'name', 'isComplete'],
-    raw: true
-  }).then((todo) => res.render('edit', { todo }))
-})
-
-app.put('/todos/:id', (req, res) => {
-  const { name, isComplete } = req.body
-  const id = req.params.id
-
-  return Todo.update({ name, isComplete: isComplete === 'completed' }, { where: { id } }).then(() =>
-    res.redirect(`/todos/${id}`)
-  )
-})
-
-app.delete('/todos/:id', (req, res) => {
-  const id = req.params.id
-
-  return Todo.destroy({ where: { id } }).then(() => res.redirect('/todos'))
-})
+app.use(router) // 將 request 導入路由器
 
 app.listen(port, () => {
   console.log(`http://localhost:${port}/todos`)
